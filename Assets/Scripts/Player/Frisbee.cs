10e82f0fa6cee4d9d322a90c88914ff10811d266
Sebastien Feser <sedukafe@gmail.com>
2018-12-13 12:19:40 +0100

    Player Movements & Begin frisbee movements

---
+++ Assets/Scripts/Player/Frisbee.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Frisbee : MonoBehaviour {
+    private Vector2 frisbeeSpeed;
+    public Vector2 FrisbeeSpeed
+    {
+        set { frisbeeSpeed = value; }
+    }
+    [SerializeField] Rigidbody2D frisbeeRigidbody;
+
+	void Start () {
+        frisbeeRigidbody.velocity = frisbeeSpeed;
+	}
+	
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        Debug.Log(collision);
+        if (collision.tag == "Wall")
+        {
+            Debug.Log("lol");
+            frisbeeRigidbody.velocity = new Vector2(-(frisbeeRigidbody.velocity.x), frisbeeRigidbody.velocity.y);
+        }
+    }
+}
