da536533933c2e9041d41a7d2135829f22bd2ae8
Stephen Grosjean <stephen-gm@hotmail.com>
2018-12-14 20:14:30 +0100

    Added Images
    + Freeze effect

---
+++ Assets/Scripts/Other/GizmoPoint.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GizmoPoint : MonoBehaviour {
+    [SerializeField] private float radius;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(transform.position, radius);
+    }
+}
