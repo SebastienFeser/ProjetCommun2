e5b2bc3ed2a09a42e6a0f50f2ffda883ac7b0a6a
Stephen Grosjean <stephen-gm@hotmail.com>
2018-12-16 20:43:53 +0100

    Added money

--- Assets/Scripts/Level/FrisbeeChanger.cs
+++ Assets/Scripts/Level/FrisbeeChanger.cs
@@ -2,19 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class FrisbeeChanger : MonoBehaviour {
     [SerializeField] private GameObject laser;
     private LaserMovements laserMovementScript;
     [SerializeField] private EnemyController.deathType type;
-    [SerializeField] private TextMeshProUGUI text;
+    [SerializeField] private Image imageCurrent;
+    [SerializeField] private Sprite normal, ice, fire, electric, gaz;
+
 	void Start () {
         laserMovementScript = laser.GetComponent<LaserMovements>();
 	}
 	
 	public void ModifyType()
     {
-        text.text = "Current frisbee : " + type.ToString();
+        switch (type)
+        {
+            case EnemyController.deathType.normal:
+                imageCurrent.sprite = normal;
+                break;
+
+            case EnemyController.deathType.ice:
+                imageCurrent.sprite = ice;
+                break;
+
+            case EnemyController.deathType.fire:
+                imageCurrent.sprite = fire;
+                break;
+
+            case EnemyController.deathType.electric:
+                imageCurrent.sprite = normal;
+                break;
+
+            case EnemyController.deathType.gaz:
+                imageCurrent.sprite = gaz;
+                break;
+        }
         laserMovementScript.Type = type;
     }
 }
