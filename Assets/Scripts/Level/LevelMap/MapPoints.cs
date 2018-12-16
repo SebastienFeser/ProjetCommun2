56a0724d1945f01a4b95f4e413a2c0ba0d92613c
Sebastien Feser <sedukafe@gmail.com>
2018-12-16 20:39:46 +0100

    Le systeme de map bug vraiment c'est bizarre...

--- Assets/Scripts/Level/LevelMap/MapPoints.cs
+++ Assets/Scripts/Level/LevelMap/MapPoints.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class MapPoints : MonoBehaviour {
 
-    [SerializeField] GameObject leftPoint;
-    [SerializeField] GameObject rightPoint;
-    [SerializeField] GameObject upPoint;
-    [SerializeField] GameObject downPoint;
+    [SerializeField] public GameObject leftPoint;
+    [SerializeField] public GameObject rightPoint;
+    [SerializeField] public GameObject upPoint;
+    [SerializeField] public GameObject downPoint;
 
-    [SerializeField] GameObject player;
-    [SerializeField] float playerSpeed;
-
-    bool canMove;
+    bool canMove = true;
 
 	// Use this for initialization
 	void Start () {
@@ -21,38 +18,8 @@ public class MapPoints : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if ((gameObject.transform.position.x - player.transform.position.x < 0.05f || gameObject.transform.position.x - player.transform.position.x > -0.05f) && (gameObject.transform.position.y - player.transform.position.y < 0.05f || gameObject.transform.position.y - player.transform.position.y > -0.05f))
-        {
-            canMove = true;
-        }
-
-		
+       
 	}
 
-    private void FixedUpdate()
-    {
-        if (canMove)
-        {
-            if (Input.GetAxis("Horizontal") > 0 && rightPoint != null)
-            {
-                Debug.Log("Go right");
-                transform.position = Vector3.Lerp(transform.position, rightPoint.transform.position, playerSpeed);
-            }
-            if (Input.GetAxis("Horizontal") < 0 && leftPoint)
-            {
-                transform.position = Vector3.Lerp(transform.position, leftPoint.transform.position, playerSpeed);
-
-            }
-            if (Input.GetAxis("Vertical") > 0 && upPoint != null)
-            {
-                transform.position = Vector3.Lerp(transform.position, upPoint.transform.position, playerSpeed);
-
-            }
-            if (Input.GetAxis("Vertical") < 0 && downPoint != null)
-            {
-                transform.position = Vector3.Lerp(transform.position, downPoint.transform.position, playerSpeed);
-
-            }
-        }
-    }
+    
 }
